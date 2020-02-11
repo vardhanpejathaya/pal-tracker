d8cd3ee53aeb5aae6a2b31fcb0dac86981df06e2
vardhan <vardhan.56@wipro.com>
2020-02-11 14:30:16 -0800

    Added EN configurations

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String helloMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String helloMessage) {
+        this.helloMessage=helloMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return helloMessage;
     }
 }
